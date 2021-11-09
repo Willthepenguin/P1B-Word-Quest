cb7a89f590d430cba2dee03c872b3d45326f8ba7
Andrew C <93136579+Andrew335483038@users.noreply.github.com>
2021-11-08 22:10:22 -0500

    Unfinished Background ending done with comments

--- EndScreen.java
+++ EndScreen.java
@@ -21,12 +21,21 @@ public class EndScreen extends World
         image.scale(image.getWidth()*8/5, image.getHeight()*6/5);
         getBackground().drawImage(image, 0, 0);
         
-        getBackground().drawImage(new GreenfootImage("Thanks for Playing!" ,32, null, null), 275, 200);
-        getBackground().drawImage(new GreenfootImage("Your Score:" ,32, null, null), 325, 300);
+        getBackground().drawImage(new GreenfootImage("Thanks for Playing!" ,32, null, null), 275, 100);
+        getBackground().drawImage(new GreenfootImage("Your Score:" ,32, null, null), 325, 200);
         //Score
         // getBackground().drawImage(new GreenfootImage(score ,32, null, null), 350, 350);
         
         getBackground().drawImage(new GreenfootImage("Our Personal High Scores!" ,32, null, null), 250, 450);
+        
+        // If score is greater than all of our scores,
+        // Says "You are a legend!
+        
+        // If score worse than all 3 of our scores,
+        // You suck!
+        
+        // If scores in between ours,
+        // You are average!
         
         
         
