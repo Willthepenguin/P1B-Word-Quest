75e75f0dacc7a373391572ee772fa29782895c9f
JackyFT <93136457+JackyFT@users.noreply.github.com>
2021-11-10 08:57:55 -0500

    Update Transition1.java

--- Transition1.java
+++ Transition1.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Transition1 extends World
 {
     private Button confirm;
-    private GreenfootSound transition=new GreenfootSound("P5 OST 09 Beneath the Mask  Instrumental Version.mp3");
+    
     /**
      * Constructor for objects of class Transition1.
      * 
@@ -18,8 +18,7 @@ public class Transition1 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-        transition.playLoop();
-        transition.setVolume(50);
+        
         confirm = new Button("I understand");
         addObject(confirm, 700, 550);
     }
@@ -31,7 +30,7 @@ public class Transition1 extends World
     private void mouseClicked(){
         if (Greenfoot.mouseClicked(confirm)){
             Greenfoot.setWorld(new AssembleWorld());
-            transition.stop();
+            
         }
     }
 }
